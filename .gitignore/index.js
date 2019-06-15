01188ef1b1581d9946bec7e92c356944ffc88414
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2019-06-15 20:36:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,10 +12,10 @@ bot.on("ready", function() {
 
 bot.on('message', msg => {
   if(msg.content[0] === PREFIX) {
-    if(msg.content === PREFIX + 'role1') {
-      let role = msg.guild.roles.find('name', 'role1')
+    if(msg.content === PREFIX + 'game') {
+      let role = msg.guild.roles.find('name', 'Game')
 
-      if(msg.member.roles.find('name', 'role1')) {
+      if(msg.member.roles.find('name', 'Game')) {
         msg.member.removeRole(role)
         msg.reply(':x: Ce rôle vous a été enlevé.')
       }
